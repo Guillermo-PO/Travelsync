0989e0512711955b09115f8f3a4fe928664ece26
Guillermo-PO <sebastianpompaortiz@gmail.com>
2026-07-06 19:33:38 -0600

    Update cache name to tripsync-cache-v9

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    TripSync — Service Worker
    ========================================================================== */
 
-const CACHE_NAME = "tripsync-cache-v8";
+const CACHE_NAME = "tripsync-cache-v9";
 const OFFLINE_URL = "offline.html";
 
 const APP_SHELL = [
